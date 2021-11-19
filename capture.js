7afc20bda10705045dc55d6e2c3f81abd8ce62e4
cnouguier <christophe.nouguier@free.fr>
2021-11-20 00:33:36 +0100

    revert: try/catch refactoring

--- capture.js
+++ capture.js
@@ -46,108 +46,116 @@ async function getLayerCategoryId (page, layerId) {
  */
  export async function capture (parameters) {
   console.log('capture requested with the following parameters: ', _.omit(parameters, 'jwt'))
+  // Instanciate the browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--headless',
+      '--hide-scrollbars',
+      '--enable-webgl',
+      '--disable-gpu',
+      '--disable-dev-shm-usage'
+    ]
+  })
+  // Create the page and listen to page errors
+  const page = await browser.newPage()
+  page.on('error', error => { 
+    console.error('error happen at the page: ', error) 
+  })
+  page.on('pageerror', error => { 
+    console.error('pageerror occurred: ', error) 
+  })
+  // Process the page viewport
+  await page.setViewport({
+    width: _.get(parameters, 'size.width', 1024),
+    height: _.get(parameters, 'size.height', 768),
+    deviceScaleFactor: 1
+  })
+  // Process the local storage items
+  await page.evaluateOnNewDocument(parameters => {
+    localStorage.clear();
+    localStorage.setItem('kano-jwt', parameters.jwt)
+    localStorage.setItem('kano-welcome', false)
+    // set the bbox view if not needed to render additional features
+    if (parameters.bbox && !parameters.features) {
+      const view = JSON.stringify({ 
+        west: parameters.bbox[0],
+        south: parameters.bbox[1],
+        east: parameters.bbox[2],
+        north: parameters.bbox[3]
+      })
+      localStorage.setItem('kano-mapActivity-view', view)
+    }
+  }, parameters)
+  // Goto the kano url
   try {
-    // Instanciate the browser
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: [
-        '--no-sandbox',
-        '--headless',
-        '--hide-scrollbars',
-        '--enable-webgl',
-        '--disable-gpu',
-        '--disable-dev-shm-usage'
-      ]
-    })
-    // Create the page and listen to page errors
-    const page = await browser.newPage()
-    page.on('error', error => { 
-      console.error('error happen at the page: ', error) 
-    })
-    page.on('pageerror', error => { 
-      console.error('pageerror occurred: ', error) 
-    })
-    // Process the page viewport
-    await page.setViewport({
-      width: _.get(parameters, 'size.width', 1024),
-      height: _.get(parameters, 'size.height', 768),
-      deviceScaleFactor: 1
-    })
-    // Process the local storage items
-    await page.evaluateOnNewDocument(parameters => {
-      localStorage.clear();
-      localStorage.setItem('kano-jwt', parameters.jwt)
-      localStorage.setItem('kano-welcome', false)
-      // set the bbox view if not needed to render additional features
-      if (parameters.bbox && !parameters.features) {
-        const view = JSON.stringify({ 
-          west: parameters.bbox[0],
-          south: parameters.bbox[1],
-          east: parameters.bbox[2],
-          north: parameters.bbox[3]
-        })
-        localStorage.setItem('kano-mapActivity-view', view)
-      }
-    }, parameters)
-    // Goto the kano url
     await page.goto(parameters.url)
     await page.waitForTimeout(500)
-    // Process the layers
-    if (parameters.layers) {
-      // Open the catalog
-      await clickSelector(page, '#right-opener')
-      await page.waitForTimeout(250)
-      let openedCategories = []
-      for (let i = 0; i < parameters.layers.length; ++i) {
-        const layerId = parameters.layers[i]
-        const categoryId = await getLayerCategoryId(page, layerId)
-        if (!openedCategories.includes(categoryId)) {
-          await clickSelector(page, `#${categoryId}`)
-          openedCategories.push(categoryId)
-        }
-        let layerSelector = `#${layerId}`
-        if (categoryId !== 'k-catalog-panel-base-layers') layerSelector += ' .q-toggle'
-        await clickSelector(page, layerSelector)
+  } catch (error) {
+    console.error(`Goto to ${parameters.url} failed: ${error}`)
+    return null
+  }
+  // Process the layers
+  if (parameters.layers) {
+    // Open the catalog
+    await clickSelector(page, '#right-opener')
+    await page.waitForTimeout(250)
+    let openedCategories = []
+    for (let i = 0; i < parameters.layers.length; ++i) {
+      const layerId = parameters.layers[i]
+      const categoryId = await getLayerCategoryId(page, layerId)
+      if (!openedCategories.includes(categoryId)) {
+        await clickSelector(page, `#${categoryId}`)
+        openedCategories.push(categoryId)
       }
+      let layerSelector = `#${layerId}`
+      if (categoryId !== 'k-catalog-panel-base-layers') layerSelector += ' .q-toggle'
+      await clickSelector(page, layerSelector)
     }
-    // Process the features
-    if (parameters.features) {
-      // Create tmp directory if needed
-      createTmpDir()
-      // Define a temporary feature file name
-      const tmpGeoJsonFile = 'features-' + crypto.randomBytes(4).readUInt32LE(0) + '.json'
-      // Build the GeoJson content
-      let geoJson = {
-        type: 'FeatureCollection',
-        features: parameters.features
-      }
-      if (parameters.bbox) geoJson.bbox = parameters.bbox
-      // Write the file for droping it
-      writeTmpFile(tmpGeoJsonFile,  JSON.stringify(geoJson))
+  }
+  // Process the features
+  if (parameters.features) {
+    // Create tmp directory if needed
+    createTmpDir()
+    // Define a temporary feature file name
+    const tmpGeoJsonFile = 'features-' + crypto.randomBytes(4).readUInt32LE(0) + '.json'
+    // Build the GeoJson content
+    let geoJson = {
+      type: 'FeatureCollection',
+      features: parameters.features
+    }
+    if (parameters.bbox) geoJson.bbox = parameters.bbox
+    // Write the file for droping it
+    writeTmpFile(tmpGeoJsonFile,  JSON.stringify(geoJson))
+    try {
       const loader = await page.$('#dropFileInput')
       await loader.uploadFile(path.join(tmpDir, tmpGeoJsonFile))
       await page.waitForTimeout(250)
-      // Delete the file
-      deleteTmpFile(tmpGeoJsonFile)
+    } catch (error) {
+      console.error(`Upload features file failed: ${error}`)
     }
-    // Hide the layout components
-    await page.evaluate(() => {
-      window.$store.set('topPane.content', null)
-      window.$store.set('bottomPane.content', null)
-      window.$store.set('rightPane.content', null)
-      window.$store.set('leftPane.content', null)
-      window.$store.set('fab.actions', [])
-    })
-    // Wait for the network to be idle
-    await page.waitForNetworkIdle({ timeout: 15000 })
-    await page.waitForTimeout(500)
-    // Take the screenshot
-    const buffer = await page.screenshot({ fullPage: true, type: 'png' })
-    await browser.close()  
-    // Return the image as a buffer
-    return buffer
-  } catch (error) {
-    console.error(`An error has occured: ${error}`)
-    return null
+    // Delete the file
+    deleteTmpFile(tmpGeoJsonFile)
   }
+  // Hide the layout components
+  await page.evaluate(() => {
+    window.$store.set('topPane.content', null)
+    window.$store.set('bottomPane.content', null)
+    window.$store.set('rightPane.content', null)
+    window.$store.set('leftPane.content', null)
+    window.$store.set('fab.actions', [])
+  })
+  // Wait for the network to be idle
+  try {
+    await page.waitForNetworkIdle({ timeout: 15000 })
+    await page.waitForTimeout(1000)
+  } catch(error) {
+    console.error(`Wait for networkd idle failed: ${error}`)
+  }
+  // Take the screenshot
+  const buffer = await page.screenshot({ fullPage: true, type: 'png' })
+  await browser.close()  
+  // Return the image as a buffer
+  return buffer
 }
