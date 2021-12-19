5ccb6fca28c7948d87235bf767382ce6f55e1a20
cnouguier <christophe.nouguier@free.fr>
2021-12-19 15:19:58 +0100

    ci: trying to build docker container and helm chart [helm]

--- .travis.helm.sh
+++ .travis.helm.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-NAME=`yq e '.name' chart/Chart.yaml`
-VERSION=`yq e '.version' chart/Chart.yaml`
-
-cp -R chart $NAME
-
-helm lint $NAME
-helm dep update $NAME
-helm package $NAME
+helm lint chart
+helm dep update chart
+helm package chart
 
 mkdir -p $HOME/.config/rclone
 envsubst < rclone.conf.tpl > $HOME/.config/rclone/rclone.conf
 
-
-
+NAME=`yq e '.name' chart/Chart.yaml`
+VERSION=`yq e '.version' chart/Chart.yaml`
 rclone copy --progress $NAME-$VERSION.tgz artifacts-host:/kalisio-artifacts/charts
