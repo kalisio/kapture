3583c6c09ec3fd3895a3a2e7fb7f38f7d45c72fc
Luc Claustres <luc.claustres@orange.fr>
2024-04-23 12:44:47 +0200

    wip: Refactor CI to use GitHub actions and generic bash scripts (#20)

---
+++ scripts/run_tests.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
+. "$THIS_DIR/kash/kash.sh"
+
+## Parse options
+##
+
+NODE_VER=16
+MONGO_VER=4
+CI_STEP_NAME="Run tests"
+CODE_COVERAGE=false
+while getopts "m:n:cr:" option; do
+    case $option in
+        n) # defines node version
+            NODE_VER=$OPTARG
+             ;;
+        c) # publish code coverage
+            CODE_COVERAGE=true
+            ;;
+        r) # report outcome to slack
+            load_env_files "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_SERVICES.enc.env"
+            CI_STEP_NAME=$OPTARG
+            trap 'slack_ci_report "$ROOT_DIR" "$CI_STEP_NAME" "$?" "$SLACK_WEBHOOK_SERVICES"' EXIT
+            ;;
+        *)
+            ;;
+    esac
+done
+
+## Init workspace
+##
+
+. "$WORKSPACE_DIR/development/workspaces/services/services.sh" kapture
+
+# Need to switch from default Kano config which is local dev env
+export KANO_URL=$KANO_DEV_URL
+export KANO_JWT=$KANO_DEV_JWT
+
+## Run tests
+##
+
+run_lib_tests "$ROOT_DIR" "$CODE_COVERAGE" "$NODE_VER"
